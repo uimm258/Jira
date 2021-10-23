b98c502e76165562573b6241ae27d051644fe465
uimm258 <uimm258@gmail.com>
2021-10-22 20:59:17 -0400

    feat: add login

---
+++ __json_server_mock__/middleware.js
@@ -0,0 +1,15 @@
+module.exports = (req, res, next) => {
+  if (req.method === "POST" && req.path === "/login") {
+    console.log("hello");
+    if (req.body.username === "jira" && req.body.password === "jira1") {
+      return res.status(200).json({
+        user: {
+          token: "123",
+        },
+      });
+    } else {
+      return res.status(400).json({ message: "unknown username or password" });
+    }
+  }
+  next();
+};
---
+++ src/screens/login/login.tsx
@@ -0,0 +1,40 @@
+import React, { FormEvent } from "react";
+
+const apiURL = process.env.REACT_APP_API_URL;
+
+export const LoginScreen = () => {
+  const login = (param: { username: string; password: string }) => {
+    fetch(`${apiURL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(param),
+    }).then(async (res) => {
+      if (res.ok) {
+      }
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const username = (e.currentTarget.elements[0] as HTMLInputElement).value;
+    const password = (e.currentTarget.elements[1] as HTMLInputElement).value;
+    login({ username, password });
+    console.log(username, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input type="text" id={"username"} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" id={"password"} />
+      </div>
+      <button type={"submit"}>Login</button>
+    </form>
+  );
+};
