5be05ac3585e033755225d16791e4092b40fe9b4
uimm258 <uimm258@gmail.com>
2021-10-25 18:20:06 -0400

    chore: add register and jwt

---
+++ src/auth-provider.ts
@@ -0,0 +1,41 @@
+import { User } from "screens/project-list/search-panel";
+
+const localStorageKey = "__auth_provider_token__";
+const apiURL = process.env.REACT_APP_API_URL;
+
+export const getToken = () => window.localStorage.getItem(localStorageKey);
+
+export const handleUserResponse = ({ user }: { user: User }) => {
+  window.localStorage.setItem(localStorageKey, user.token || "");
+  return user;
+};
+
+export const login = (data: { username: string; password: string }) => {
+  fetch(`${apiURL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (res) => {
+    if (res.ok) {
+      return handleUserResponse(await res.json());
+    }
+  });
+};
+
+export const register = (data: { username: string; password: string }) => {
+  fetch(`${apiURL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (res) => {
+    if (res.ok) {
+      return handleUserResponse(await res.json());
+    }
+  });
+};
+
+export const logout = () => window.localStorage.removeItem(localStorageKey);
